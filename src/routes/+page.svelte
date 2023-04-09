f790fe3801c74bb299a32b8872836c3ab1d71e39
katzkb <katsuya7kubo@gmail.com>
2023-04-10 03:28:54 +0900

    Add bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { Card, Container, Col, Row, CardHeader, CardTitle, CardBody, CardText, Button } from "sveltestrap"
+</script>
+<Container fluid class="main-container">
+  <Row class="align-items-center main-row">
+    <Col class="col-sm-1 col-md-6 offset-md-3">
+      <Card class="mb-3">
+        <CardHeader>
+          <CardTitle>トップページ</CardTitle>
+        </CardHeader>
+        <CardBody>
+          <CardText>サービスは準備中です。事前の会員登録は下記からどうぞ。</CardText>
+          <Button>会員登録</Button>
+          <Button>ログイン</Button>
+        </CardBody>
+      </Card>
+    </Col>
+  </Row>
+</Container>
+<style lang="scss">
+  :global(.main-container) {
+    height: 100vh;
+  }
+  :global(.main-row) {
+    height: 100%;
+  }
+</style>
